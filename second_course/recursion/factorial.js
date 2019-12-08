e2526ad09a0dc49215e6e6ad0b50c2442725f3d8
Ryoung27 <RRYoung89@gmail.com>
2019-12-08 12:47:33 -0600

    factorial

---
+++ second_course/recursion/factorial.js
@@ -0,0 +1,15 @@
+function factorial(num){
+    if (num === 0){
+        return 1
+    } else{
+        return num * factorial(num - 1);
+    }
+}
+
+var factorial = function(n) {
+    if(n == 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1);
+    }
+}
