cb8970166cdaf7bf4df61dcaf2a109e478df420e
Ryoung27 <RRYoung89@gmail.com>
2019-12-10 18:57:40 -0600

    classes

---
+++ second_course/data_structures/class.js
@@ -0,0 +1,16 @@
+class Student{
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+    }
+}
+
+let firstStudent = new Student("Blue", "Steele", 10);
+let secondStudent = new Student("Red", "Forman", 11);
+
+class Tree {
+
+}
+
+new Tree
