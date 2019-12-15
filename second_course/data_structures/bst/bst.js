febec8a74e8f446147bc2e43fa71f1f3a65803e4
Ryoung27 <RRYoung89@gmail.com>
2019-12-15 08:57:10 -0600

    tree trav

--- second_course/data_structures/bst/bst.js
+++ second_course/data_structures/bst/bst.js
@@ -82,6 +82,20 @@ class BinarySearchTree{
         if(!found) return undefined;
         return current;
     }
+
+    BFS(){
+        let data = [],
+            queue = [],
+            node = this.root;
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
 
 
