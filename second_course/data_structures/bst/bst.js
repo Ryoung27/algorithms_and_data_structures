94648f50abf0350381fa077e15441bc97e86fa92
Ryoung27 <RRYoung89@gmail.com>
2019-12-15 07:58:09 -0600

    find

--- second_course/data_structures/bst/bst.js
+++ second_course/data_structures/bst/bst.js
@@ -10,6 +10,78 @@ class BinarySearchTree{
     constructor(){
         this.root = null;
     }
+
+    insert(value){
+        let newNode = new Node(value);
+        if(value === current.value) return undefined;
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        } else{
+            let current = this.root;
+            while(true){
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this
+                    } else{
+                        current = current.left;
+                    }
+                } else if(value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this
+                    } else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    find(value){
+        let current = this.root;
+        let found = false;
+        if(this.root === null){
+            return undefined;
+        } else if(this.root === value){
+            return this.root;
+        } else{
+            let current = this.root;
+            while(true){
+                if(value > current.value){
+                    if(current.right === value){
+                        return this;
+                    } else{
+                        current = current.right;
+                    }
+                } else if(value < current.value){
+                    if(current.left === value){
+                        return this
+                    } else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    iFind(value){
+        if(this.root === null) return false;
+        let current = this.root;
+        let found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else{
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
 }
 
 
