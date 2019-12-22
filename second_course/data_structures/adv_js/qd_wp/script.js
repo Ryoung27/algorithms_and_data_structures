75052ff1e5c218139a8512a129f432a6aacb75cb
Ryoung27 <RRYoung89@gmail.com>
2019-12-22 09:25:18 -0600

    fundamental functions

--- second_course/data_structures/adv_js/qd_wp/script.js
+++ second_course/data_structures/adv_js/qd_wp/script.js
@@ -1,1 +1,33 @@
-alert("How about that");
+function sayHello(){
+	console.log("Hello");
+}
+
+
+let sayGoodbye = function sayGoodbye(){
+	console.log("Good bye");
+}
+
+function sing(song, lyrics){
+	console.log(song);
+	console.log(lyrics);
+}
+
+function multiply(a, b){
+	console.log(a * b);
+	return a * b;
+}	
+
+function new_multiply(a, b){
+	if (a > 10 || b > 10){
+		return "Too large";
+	} else{
+		return a * b;
+		}
+}
+
+sayHello();
+sayGoodbye();
+sing("random title", "wth");
+multiply(5, 10);
+new_multiply(1, 1);
+multiply(100, 100);
