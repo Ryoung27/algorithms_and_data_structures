e30f9615f504d643184cf1b85867db48e106b5b9
Ryoung27 <RRYoung89@gmail.com>
2019-12-23 08:36:16 -0600

    more basics

---
+++ second_course/data_structures/adv_js/qd_wp/array.js
@@ -0,0 +1,9 @@
+let list = ["tiger", "cat", "bear", "bird"];
+list[1];
+
+let numbers = [1, 2, 3, 4]
+let booleans = [true, false, true]
+
+let functionList = [function apple() {
+console.log('apple');
+}]
