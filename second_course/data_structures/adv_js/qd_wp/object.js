391e51dd3c09e2cf8da0c293bacb8578472b5fce
Ryoung27 <RRYoung89@gmail.com>
2019-12-23 08:45:25 -0600

    list of objects

---
+++ second_course/data_structures/adv_js/qd_wp/object.js
@@ -0,0 +1,19 @@
+let user = {
+	name: "Richie',
+	age: 30,
+	hobby: "software development",
+}
+
+
+
+let user_list = [
+	{
+		usename: "R",
+		email: "bank@blank.com"
+	},
+	{
+		username: "K",
+		email: "more@blank.com"
+	}
+
+]
